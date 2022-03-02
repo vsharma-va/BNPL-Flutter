cf48a9e68ee7f7042fa42b4e70a05b189d628741
Vaibhav Sharma <yavsvaibhav@gmail.com>
2022-03-02 19:28:43 +0530

    V2

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 
-import 'FirstScreen/splash.dart';
+import 'amplifyconfiguration.dart';
+import 'screens/splash.dart';
+import './screens/auth/components/errorSnackBar.dart';
 
 void main() {
   runApp(BNPL());
 }
 
-class BNPL extends StatelessWidget {
+class BNPL extends StatefulWidget {
   const BNPL({Key? key}) : super(key: key);
+
+  @override
+  State<BNPL> createState() => _BNPLState();
+}
+
+class _BNPLState extends State<BNPL> {
+  bool _amplifyConfigured = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+      await Amplify.addPlugins([AmplifyAuthCognito()]);
+      await Amplify.configure(amplifyconfig);
+      setState(() {
+        _amplifyConfigured = true;
+      });
+    } catch (e) {
+      context.showErrorSnackBar(message: e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
